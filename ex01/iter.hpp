42e6a796192dd2d071c8d7a5903af21ccaaa31d3
mercedesdelcastillo <meru334@gmail.com>
2026-08-19 17:59:58 +0200

    const function, falta trabajo

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: medel-ca <medel-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 10:27:16 by medel-ca          #+#    #+#             */
-/*   Updated: 2026/08/07 12:57:54 by medel-ca         ###   ########.fr       */
+/*   Updated: 2026/08/19 13:26:18 by medel-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <iostream>
 # include <stdint.h>
 
-template <typename T> void f(T &val)
+/*template <typename T> void f(T &val)
 {
     val += val;
     std::cout << val << std::endl;
@@ -24,12 +24,13 @@ template <typename T> void f(T &val)
 template <typename T> void f(T const &val)
 {
     std::cout << val << std::endl;
-}
+}*/
 
-template <typename T> void iter(T *adr, size_t size, void (*f)(T &val))
+template <typename T,typename F>
+void iter(T *adr, size_t const size, F function)
 {
     for(size_t i = 0; i < size; i++)
-        f(adr[i]);
+        function(adr[i]);
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,21 +6,47 @@
 /*   By: medel-ca <medel-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 10:04:17 by medel-ca          #+#    #+#             */
-/*   Updated: 2026/08/07 13:00:05 by medel-ca         ###   ########.fr       */
+/*   Updated: 2026/08/19 13:28:31 by medel-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
 
-int main( void )
+/*int main( void )
 {
     int array[] = {1, 2, 3, 4, 5};
-    iter(array, sizeof(array) / sizeof(array[0]), f);
     iter(array, sizeof(array) / sizeof(array[0]), f);
     char const array1[] = { 'a', 'b', 'c'};
     iter(array1, sizeof(array1) / sizeof(array1[0]), f);
     std::string array2[] = { "42", "adios", "hola"};
     iter(array2, sizeof(array2) / sizeof(array2[0]), f);
     return 0;
+}*/
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+template <typename T>
+void f(T& val)
+{
+    std::cout << val << std::endl;
+}
+
+int main() {
+    int tab[] = { 0, 1, 2, 3, 4 };
+    const int tab2[] = { 0, 1, 2, 3, 4 };
+
+    const int len = 5;
+
+    iter( tab, len, print<const int> );
+    iter( tab, len, print<int> );
+    iter( tab2, len, f<const int> );
+    iter( tab2, len, print<const int> );
+
+  return 0;
 }
