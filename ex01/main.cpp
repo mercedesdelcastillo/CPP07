79d56529f219acebde7c92cf316672411ca97c42
Meru <meru334@gmail.com>
2026-08-21 12:31:46 +0200

    Template function const & non const

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,24 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: medel-ca <medel-ca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: medel-ca <medel-ca@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 10:04:17 by medel-ca          #+#    #+#             */
-/*   Updated: 2026/08/07 13:00:05 by medel-ca         ###   ########.fr       */
+/*   Updated: 2026/08/21 12:31:02 by medel-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
 
-int main( void )
+template< typename T >
+void print( T const &x )
 {
-    int array[] = {1, 2, 3, 4, 5};
-    iter(array, sizeof(array) / sizeof(array[0]), f);
-    iter(array, sizeof(array) / sizeof(array[0]), f);
-    char const array1[] = { 'a', 'b', 'c'};
-    iter(array1, sizeof(array1) / sizeof(array1[0]), f);
-    std::string array2[] = { "42", "adios", "hola"};
-    iter(array2, sizeof(array2) / sizeof(array2[0]), f);
-    return 0;
+    std::cout << x << std::endl;
+    return;
+}
+
+template <typename T>
+void doubleval(T& val)
+{
+    val +=val;
+    std::cout << val << std::endl;
+}
+
+int main() {
+    int tab[] = { 0, 1, 2, 3, 4 };
+    const int tab2[] = { 00, 10, 20, 30, 40 };
+
+    const int len = 5;
+
+    std::cout << "Print function (const)" << std::endl;
+    iter( tab, len, print<const int> );
+    std::cout << "Print function ()" << std::endl;
+    iter( tab, len, print<int> );
+    std::cout << "Double Val function (non const)" << std::endl;
+    iter( tab, len, doubleval<int> );
+    std::cout << "Print function (const)" << std::endl;
+    iter( tab2, len, print<const int> );
+    std::cout << "Print function ()" << std::endl;
+    iter( tab2, len, print<int> );
+   
+
+  return 0;
 }
